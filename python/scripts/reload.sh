421cfafdf02849d2a04c94271492da1a4b8a172a
Yuta Kasai <kasai.yuta0810@gmail.com>
2020-06-05 09:39:18 -0700

    Manage mysql and nginx by copy

--- python/scripts/reload.sh
+++ python/scripts/reload.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+if [[ ! -e /etc/nginx/nginx.conf.backup ]]; then
+  sudo cp /etc/nginx/nginx.conf /etc/uginx/nginx.conf.backup
+fi
+sudo cp ./etc/nginx/nginx.conf /etc/nginx/nginx.conf
 sudo service nginx reload
+
+if [[ ! -e /etc/mysql/mysql.conf.d/mysqld.cnf.backup ]]; then
+  sudo cp /etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf.backup
+fi
+sudo cp ./etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo service mysql restart 
