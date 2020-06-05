2b0857072e9bd921aa4ef9c5842a18445ac1afaf
Yuta Kasai <kasai.yuta0810@gmail.com>
2020-06-05 05:22:17 -0700

    Increase python workers by 4 : 1225

--- python/scripts/start_gunicorn.sh
+++ python/scripts/start_gunicorn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source scripts/export_env.sh \
-&& ./venv/bin/gunicorn app:app -b '127.0.0.1:8080' --access-logfile=-
+&& ./venv/bin/gunicorn app:app -b '127.0.0.1:8080' --access-logfile=- -w 4
